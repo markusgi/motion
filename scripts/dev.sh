689b0f3d08289c4bb711e86291f636c35a3651d5
Markus Giger <markusgig@gmail.com>
2020-10-16 14:21:24 +0200

    change in scripts

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,4 +2,6 @@
 python -c "import time; time.sleep(3)"
 python manage.py makemigrations
 python manage.py migrate
+
+rm -rf /frontend/build/* && cp -r /frontend_tmp/* /frontend
 python manage.py runserver 0.0.0.0:8000
