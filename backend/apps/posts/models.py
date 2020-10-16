91f1696c3cdacef8146915df0889861e8688a5a4
Markus Giger <markusgig@gmail.com>
2020-10-16 14:10:22 +0200

    migrations

--- backend/apps/posts/models.py
+++ backend/apps/posts/models.py
@@ -1,15 +1,13 @@
-from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.db import models
-
-User = get_user_model()
 
 
 class Post(models.Model):
     title = models.CharField(max_length=60)
     text_content = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    author_id = models.ForeignKey(to=User, related_name='posts_from_this_user', on_delete=models.CASCADE, null=True)
-    fans_of_post = models.ManyToManyField(to=User, related_name='liked_posts', blank=True)
+    author = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name='posts_from_this_user', on_delete=models.CASCADE, null=True)
+    fans_of_post = models.ManyToManyField(to=settings.AUTH_USER_MODEL, related_name='liked_posts', blank=True)
 
     class Meta:
         ordering = ['-id']
